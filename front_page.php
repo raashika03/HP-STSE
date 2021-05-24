598f52f72709fffb6b49d4caa73617842d61a1b9
Rashika Rathi <b18081@students.iitmandi.ac.in>
2021-05-24 07:26:41 +0530

    Add files via upload

---
+++ front_page.php
@@ -0,0 +1,17 @@
+
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class=""enctype="multipart/form-data" action="register.php" method="post" target="_blank">
+        <input type="button" value="Register for student" onclick="window.location.href='student/registration-form.php'" />
+        <input type="button" value="Register for admin" onclick="window.location.href='admin/register_a.php'" />
+        <input type="button" value="Register for principal" onclick="window.location.href='princy/register_p.php'" />
+    </form>
+  </body>
+</html>
